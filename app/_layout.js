0d1eb7a62e86ae6e48a67b611e10ff89342c3292
Gerardo Resendiz Vega <gerardoresendizvega@gmail.com>
2025-04-21 16:06:08 -0600

    cambios en _layout

--- app/_layout.js
+++ app/_layout.js
@@ -6,7 +6,7 @@ export default function Layout() {
 
     return (
         <View style={{ flex: 1, backgroundColor: "#000" }}>
-           
+            
             <Logo />
             <Slot />
         </View>
